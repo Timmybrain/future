937597ccc4969258162355b6ab60bf4f3f7b723d
Timmybrain <info@timmybrain.com>
2018-04-13 16:34:38 +0100

    Update all changes

---
+++ admin/templates/umca/includes/umca.navmenu.php
@@ -0,0 +1,80 @@
+<!-- Ownavigation -->
+		<nav class="navbar ownavigation">
+			<!-- Container -->
+			<div class="container">
+				<div class="navbar-header">
+					<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+						<span class="sr-only">Toggle navigation</span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+					</button>
+					<a class="navbar-brand" href="index.html"><img src="<?=$future->theme_assets?>/images/logo.png" alt="logo" height="75" width="75"></a>
+				</div>
+				<div class="submit-btn">
+					<a href="#" title="SignUp">Register as Member</a>
+				</div>
+				<div id="navbar" class="navbar-collapse collapse navbar-right">
+					<ul class="nav navbar-nav navbar-right">
+						<li class="dropdown">
+							<a href="index.html" title="Home">Home</a>
+							<!--i class="ddl-switch fa fa-user"></i>
+							<ul class="dropdown-menu">
+								<li><a href="index.html" title="Homepage 01">Homepage 01</a></li>
+								<li><a href="index.html" title="Homepage 02">Homepage 02</a></li>
+							</ul-->
+						</li>
+						<li><a href="leadership.html" title="Leadership">LeaderShip</a></li>
+						<li class="dropdown">
+							<a href="fellowships.html" class="dropdown-toggle" role="button" aria-haspopup="true" aria-expanded="false">Fellowships</a>
+							<i class="ddl-switch fa fa-angle-down"></i>
+							<ul class="dropdown-menu">
+								<li>
+									<a href="mensFellowships.html" title="Men's Fellowship">Men's Fellowship</a>
+								</li>
+								<li>
+									<a href="ladiesFELLOWSHIP.html" title="Services List">Ladies's Fellowship</a>
+								</li>
+								<li>
+									<a href="fellowships.html" title="Services">Couple's Koinonia</a>
+								</li>
+								<li>
+									<a href="foyca.html" title="Fellowship of Young Christain Adult">FOYCA</a>
+								</li>
+								<li>
+									<a href="fellowships.html" title="Services">Youth Fellowship</a>
+								</li>
+							</ul>
+						</li>
+						<li class="dropdown">
+							<a href="department.html" class="dropdown-toggle" role="button" aria-haspopup="true" aria-expanded="false">Departments</a>
+							<i class="ddl-switch fa fa-angle-down"></i>
+							<!--ul class="dropdown-menu">
+								<li><a href="fellowships.html" title="Dunamis Choir">Dunamis Choir</a></li>
+								<li><a href="ladiesFellowship.html" title="Ushering">Ushering</a></li>
+								<li><a href="fellowships.html" title="Drama Unit">Drama Unit</a></li>
+								<li><a href="fellowships.html" title="Writers' Unit">Writers' Unit</a></li>
+								<li><a href="fellowships.html" title="Arts Unit">Arts Unit</a></li>
+								<li><a href="fellowships.html" title="Sunday School">Sunday School</a></li>
+							</ul>
+						</li>
+						<li class="dropdown">
+							<a href="#" class="dropdown-toggle btn-disabled" role="button" aria-haspopup="true" aria-expanded="false">Resources</a>
+							<i class="ddl-switch fa fa-angle-down"></i>
+							<ul class="dropdown-menu">
+								<li><a href="blog.html" title="Blog Single Page">Blog</a></li>
+								<li><a href="blog-single.html" title="Blog Single Audio">Others</a></li>
+							</ul>
+						</li-->
+						<li><a href="aboutus.html" title="About">About</a></li>
+						<!--li><a href="contactus.html" title="Contact Us">Contact</a></li-->
+					</ul>
+				</div>
+				<div id="loginpanel" class="desktop-hide">
+					<div class="right" id="toggle">
+						<a id="slideit" href="#slidepanel"><i class="fo-icons fa fa-inbox"></i></a>
+						<a id="closeit" href="#slidepanel"><i class="fo-icons fa fa-close"></i></a>
+					</div>
+				</div>
+			</div><!-- Container /- -->
+		</nav><!-- Ownavigation /- -->
